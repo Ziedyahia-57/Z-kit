bf4e006bc6049e89dd7a7c838c68b6c61b820190
Ziedyahia-57 <ziedyahia20102012@gmail.com>
2026-03-11 18:04:20 +0100

    Added a navbar component

---
+++ z-kit/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Navbar } from '/components/navbar.jsx'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
