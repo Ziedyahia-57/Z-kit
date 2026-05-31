6604390f49f59826fe5b961aa780025bb0b89fe7
Ziedyahia-57 <ziedyahia20102012@gmail.com>
2026-05-31 23:37:04 +0100

    fix(select): replace SVG icon with Disc component for improved consistency in group items

--- z-kit/src/components/select/Select.stories.jsx
+++ z-kit/src/components/select/Select.stories.jsx
@@ -95,8 +95,7 @@ export const select = {
                     <DropdownGroup>
                         <GroupTitle>Fruits</GroupTitle>
                         <GroupItem mode="icons" shortcut="ctrl a">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M12 2v13" /><path d="m16 6-4-4-4 4" /><path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8" /></svg>
-                            Apple</GroupItem>
+                            <Disc color="red" />Apple</GroupItem>
                         <GroupItem mode="icons" shortcut="ctrl o"><Disc color="orange" />Orange</GroupItem>
                         <GroupItem mode="icons" shortcut="ctrl b"><Disc color="yellow" />Banana</GroupItem>
                     </DropdownGroup>
